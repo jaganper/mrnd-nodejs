b6b99037381957b3b47da9a19217722b4418f466
hemanth.chinthalapudi <hemanth.chinthalapudi@kony.com>
2015-06-18 15:53:49 +0530

    Added IIIT proxy server ip address in request call.

--- spec/tests/HttpIntroSpec.js
+++ spec/tests/HttpIntroSpec.js
@@ -1,14 +1,15 @@
 // This test case is trying to hit an invalid URL.
 // Fix the assertions below so they all pass.
 describe("HttpIntro Test Suite", function(){
-	//var request = require('request');
-	var request = require('C:/Program Files/nodejs/node_modules/npm/node_modules/request');
+	var request = require('request');
+	// var request = require('C:/Program Files/nodejs/node_modules/npm/node_modules/request');
 	jasmine.getEnv().defaultTimeoutInterval = 5000;
 
 	it("IDontKnowBill_Gates",function(done){
     
     	request.get(
     		{url: "http://en.wikipedia.org/wiki/IDontKnowBill_Gates",
+    		proxy: "http://10.4.8.204:8080",
     		 timeout: 5000}, 
     		 function(error, response, body){
 
@@ -27,6 +28,7 @@ describe("HttpIntro Test Suite", function(){
 	    
 	    	request.get(
 	    		{url: "https://api.twitter.com/1.1/friends/list.json",
+	    		proxy: "http://10.4.8.204:8080",
 	    		 timeout: 30000}, 
 	    		 function(error, response, body){
 
@@ -45,10 +47,11 @@ describe("HttpIntro Test Suite", function(){
 	    
 	    	request.get(
 	    		{url: "http://api.openweathermap.org/data/2.5/weather?q=jaganperi",
+	    		proxy: "http://10.4.8.204:8080",
 	    		 timeout: 30000}, 
 	    		 function(error, response, body){
 
-				console.log(response);
+				// console.log(response);
 				expect(response.statusCode).toBe(404);
 				expect(response.statusMessage).toBe('Not Found');
 				expect(response.headers["content-type"]).toBe("text/html; charset=UTF-8");
@@ -64,6 +67,7 @@ describe("HttpIntro Test Suite", function(){
 	    
 	    	request.get(
 	    		{url: "http://api.openweathermap.org/data/2.5/weather?q=hyderabad",
+	    		proxy: "http://10.4.8.204:8080",
 	    		 timeout: 30000,
 	    		  json: false}, 
 	    		 function(error, response, body){
@@ -79,11 +83,12 @@ describe("HttpIntro Test Suite", function(){
 	    
 	    	request.get(
 	    		{url: "http://api.openweathermap.org/data/2.5/weather?q=hyderabad&mode=xml",
+	    		proxy: "http://10.4.8.204:8080",
 	    		 timeout: 30000,
 	    		  json: true}, 
 	    		 function(error, response, body){
 
-				console.log(response);
+				// console.log(response);
 				//expect(body.sys.country).toBe("IN");
 
 				done();
